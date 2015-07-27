c68f145130788a84304c9b2afda5182f9764d140
afriant1 <amandafriant@gmail.com>
2015-07-27 16:18:52 -0500

    Completes Part 1

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,39 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+
+    @user_input = params["input"].to_f
+
+    if @user_input >= 0
+
+      @square_root = Math.sqrt(@user_input).round(2)
+
+      else @square_root = "imaginary. Input positive number"
+
+    end
+
+    render("square_root.html.erb")
+  end
+
+  def random
+
+     @min = params["input1"].to_i
+     @max = params["input2"].to_i
+     @random = rand(@min..@max)
+
+    render("random.html.erb")
+  end
+
+  def payment
+
+     @rate = params["input1"]
+     @rate1 = @rate.to_f/10000/12
+     @term = params["input2"].to_i
+     @loan = params["input3"].to_f
+     @payment = (@loan*@rate1*((1+@rate1)**(@term*12)))/(((1+@rate1)**(@term*12))-1)
+
+    render("payment.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:input", { :controller => "calculations", :action => "square_root" })
+  get("/random/:input1/:input2", { :controller => "calculations", :action => "random" })
+  get("/payment/:input1/:input2/:input3", { :controller => "calculations", :action => "payment" })
 end
